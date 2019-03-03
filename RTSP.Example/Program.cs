bd20ce5e32882a4d840c1c1305de9e98efeb9a70
Ceiling Waffle <ceilingwaffle@gmail.com>
2019-03-03 20:32:14 +1100

    Remove unused namespace imports

--- RTSP.Example/Program.cs
+++ RTSP.Example/Program.cs
@@ -1,10 +1,5 @@
 ﻿using RTSP.Core;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace RTSP.Example
 {
