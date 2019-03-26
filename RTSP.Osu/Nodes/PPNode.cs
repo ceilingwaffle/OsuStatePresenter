97c928343f3fb6732da78f62ee915421f0f93dc5
Ceiling Waffle <ceilingwaffle@gmail.com>
2019-03-27 00:36:09 +1100

    Add pp node + upgrade to .NET 4.7.1 from 4.5.2

---
+++ RTSP.Osu/Nodes/PPNode.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace RTSP.Osu.Nodes
+{
+    class PPNode : OsuNode
+    {
+        public override Task<object> DetermineValueAsync()
+        {
+            //var pc = new OsuMemoryDataProvider.PlayContainer();
+
+            //_memoryReader.GetPlayData(pc);
+            return null;
+        }
+    }
+}
