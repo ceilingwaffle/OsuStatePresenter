da4bbce3842d17ffb79c83238e6a876331f3e7bf
Ceiling Waffle <ceilingwaffle@gmail.com>
2019-04-01 21:54:29 +1100

    Extracted Console App implementation to its own project

---
+++ ConsoleOsuImplementation/Program.cs
@@ -0,0 +1,30 @@
+﻿using DVPF.Core;
+using OsuStatePresenter;
+using System;
+
+namespace ConsoleOsuImplementation
+{
+    class Program
+    {
+        private static readonly NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            Presenter presenter = new Presenter(StateCreatedHandler);
+
+            presenter.Start();
+
+            // keep the console open
+            while (true)
+            {
+                Console.ReadKey();
+            }
+        }
+
+        static void StateCreatedHandler(State state)
+        {
+            // TODO: Do something other than log the state
+            _logger.Info("State created:\n{0}", state);
+        }
+    }
+}
