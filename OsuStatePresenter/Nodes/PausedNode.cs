51938e8f330b120fc9fcab685dd39e77d68d9df1
Ceiling Waffle <ceilingwaffle@gmail.com>
2019-04-01 18:09:20 +1100

    PausedNode improvement: Use a timer to delay the checking of the current/previous MapTime, to fix the difference between 2 MapTime states causing flicker between Paused=true/false

--- OsuStatePresenter/Nodes/PausedNode.cs
+++ OsuStatePresenter/Nodes/PausedNode.cs
@@ -1,6 +1,7 @@
-﻿using System.Threading.Tasks;
-using DVPF.Core;
+﻿using DVPF.Core;
 using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace OsuStatePresenter.Nodes
 {
@@ -8,9 +9,34 @@ namespace OsuStatePresenter.Nodes
     [StateProperty(enabled: true, name: "SongIsPaused")]
     class PausedNode : OsuNode
     {
+        // TODO: Fix bug: Jumping around in the map editor flickers Paused to true then back to false.
+        // Possible solution: Use another timer to check the "expected" time, and do a fuzzy check around that time. If it's a huge difference, still assume the map is paused.
+
+        /// <summary>
+        /// Use a timer to delay the checking of the current/previous MapTime, to fix the
+        /// "difference between 2 MapTime states" causing "flicker" between Paused=true/false.
+        /// </summary>
+        private Stopwatch _timer = new Stopwatch();
+
+        public TimeSpan MinUpdateTime { get; set; } = StatePresenter.ScannerInterval.Add(TimeSpan.FromMilliseconds(250));
+
         public override async Task<object> DetermineValueAsync()
         {
-            // TODO: Use a 1 second timer to delay the checking of the current/previous MapTime, to fix the "difference between 2 states MapTime" causing "flicker" between IsPaused=true/false states
+            if (!_timer.IsRunning)
+            {
+                _timer.Start();
+            }
+
+            var currentValue = this.GetValue();
+
+            if (_timer.Elapsed.CompareTo(MinUpdateTime) < 0 && currentValue != null) // timer time less than min time
+            {
+                // return whatever the current value is
+                // _logger.Info($"false: {_timer.Elapsed.TotalMilliseconds}, {MinUpdateTime.TotalMilliseconds}");
+                return await Task.FromResult(currentValue);
+            }
+
+            _timer.Restart();
 
             Preceders.TryGetValue(typeof(MapTimeNode), out var mapTimeNode);
             //Preceders.TryGetValue(typeof(StatusNode), out var statusNode);
@@ -29,5 +55,6 @@ namespace OsuStatePresenter.Nodes
 
             return await Task.FromResult(false);
         }
+
     }
 }
