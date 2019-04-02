3793f37a7b962ecbd6c973aaf22950bb752e36db
Ceiling Waffle <ceilingwaffle@gmail.com>
2019-04-02 11:21:33 +1100

    Using submodule OsuMemoryDataProvider by Piotrekol - and creating multiple memory reader instances to reduce blocking between nodes when reading memory

--- OsuStatePresenter/Nodes/OsuNode.cs
+++ OsuStatePresenter/Nodes/OsuNode.cs
@@ -14,133 +14,12 @@ namespace OsuStatePresenter.Nodes
 
         public OsuNode()
         {
-            // TODO: One Node can hang another Node because they share the same OsuMemoryDataProvider instance - and the Node must wait for the reader to become "available" before it can read.
+            // using dll
+            //OsuMemoryDataProvider.DataProvider.Initalize();
+            //_memoryReader = OsuMemoryDataProvider.DataProvider.Instance;
 
-            /*
-             * almost got it working, except wasn't able to type convert IOsuMemoryReader from the 2nd OsuMemoryDataProvider dll to the 1st.
-             * 
-             */
-            /*
-            var dllPath = @"C:\Users\waffle\Documents\Code\RTSP\rtspv5\RTSP.Osu\OsuMemoryDataProvider2.dll";
-            //var assembly = domain.Load("OsuMemoryDataProvider");
-            //AssemblyName name = AssemblyName.GetAssemblyName(dllPath);
-            //var domain = AppDomain.CreateDomain("OsuMemory");
-            var assembly = Assembly.LoadFile(dllPath);
-            var dpType = assembly.GetType("OsuMemoryDataProvider.DataProvider");
-            var readerType = assembly.GetType("OsuMemoryDataProvider.IOsuMemoryReader");
-
-            dpType.GetMethod("Initalize").Invoke(null, null);
-            var instanceField = dpType.GetField("Instance", BindingFlags.Public | BindingFlags.Static | BindingFlags.GetField); //  | BindingFlags.Instance
-            var instance = instanceField.GetValue(null);
-
-            var t = instance.GetType(); // QBH$pwEBMDeVPhQUOkTRZhk1zwwdrjHJZ$CZRGRU6rF7TwFRAnjNbRlZX_aju4J2vg
-
-            var customMemReader = new IOsuMemoryReader_Custom(instance);
-
-            dynamic changedObj = Convert.ChangeType(customMemReader, readerType);
-            //_memoryReader2 = changedObj.Method();
-            _memoryReader2 = changedObj;
-
-            */
-
-
-
-
-
-
-
-            //var DLL = Assembly.LoadFile(@"C:\Users\waffle\Documents\Code\RTSP\rtspv5\RTSP.Osu\OsuMemoryDataProvider2.dll");
-            //var domain = AppDomain.CreateDomain("OsuMemory");
-            //domain.DoCallBack(LoadAssemblyAndCallStaticMethod);
-            //domain.DoCallBack(PrintLoadedAssemblies);
-
-
-
-
-
-
-
-
-
-
-            //AppDomain domain = AppDomain.CreateDomain("OsuMemory");
-            ////Do other things to the domain like set the security policy
-            //string pathToDll = @"C:\Users\waffle\Documents\Code\RTSP\rtspv5\RTSP.Osu\OsuMemoryDataProvider2.dll"; //Full path to dll you want to load
-            //Type t = typeof(IOsuMemoryReader);
-            //IOsuMemoryReader myObject = (IOsuMemoryReader)domain.CreateInstanceAndUnwrap(pathToDll, "OsuMemoryDataProvider.DataProvider");
-            //Console.WriteLine(myObject);
-
-
-
-
-
-
-
-
-
-            //foreach (Type type in DLL.GetExportedTypes())
-            //{
-            //    if (type.ToString().Equals("OsuMemoryDataProvider.DataProvider"))
-            //    {
-            //        type.GetMethod("Initalize").Invoke(null, null);
-            //        FieldInfo instance = type.GetField("Instance");
-
-            //        var o = instance.GetValue(null);
-
-            //        Console.WriteLine(o.GetType());
-
-            //        break;
-            //    }
-            //}
-
-
-
-
-
-
-
-
-
-
-            //Type IReaderType = null;
-
-            //foreach (Type type in DLL.GetExportedTypes())
-            //{
-            //    if (type.ToString().Equals("OsuMemoryDataProvider.IOsuMemoryReader"))
-            //    {
-            //        IReaderType = type;
-            //        break;
-            //    }
-
-            //}
-
-            //foreach (Type type in DLL.GetExportedTypes())
-            //{
-            //    if (type.ToString().Equals("OsuMemoryDataProvider.DataProvider"))
-            //    {
-            //        type.GetMethod("Initalize").Invoke(null, null);
-            //        FieldInfo instance = type.GetField("Instance");
-
-            //        var o = instance.GetValue(null);
-
-            //        //var r = (o as IReaderType.);
-            //        Console.WriteLine(o.GetType());
-
-            //        break;
-            //    }
-            //}
-
-
-            //}
-
-
-
-
-
-
-
-            OsuMemoryDataProvider.DataProvider.Initalize();
-            _memoryReader = OsuMemoryDataProvider.DataProvider.Instance;
+            // using project reference
+            _memoryReader = new MyOsuMemoryReader();
         }
 
         // Loads StaticMethodInHere.dll to the current AppDomain and calls static method 
@@ -240,6 +119,11 @@ namespace OsuStatePresenter.Nodes
         }
 
         public void GetPlayData(PlayContainer playContainer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetRetrys()
         {
             throw new NotImplementedException();
         }
