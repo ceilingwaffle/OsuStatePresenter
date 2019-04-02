7ad6a2e6cb628c49023f7b3d4ed1af995c341505
Ceiling Waffle <ceilingwaffle@gmail.com>
2019-04-03 03:52:35 +1100

    Improved min delay between value updates in PausedNode

--- OsuStatePresenter/Nodes/PausedNode.cs
+++ OsuStatePresenter/Nodes/PausedNode.cs
@@ -5,39 +5,70 @@ using System.Threading.Tasks;
 
 namespace OsuStatePresenter.Nodes
 {
-    // TODO: Build the "paused" status into the custom Status class
+    // TODO: REFACTOR - Build the "paused" status into the custom Status class
     [StateProperty(enabled: true, name: "SongIsPaused")]
     public class PausedNode : OsuNode
     {
-        // TODO: Fix bug: Jumping around in the map editor flickers Paused to true then back to false.
-        // Possible solution: Use another timer to check the "expected" time, and do a fuzzy check around that time. If it's a huge difference, still assume the map is paused.
+        // TODO: BUG - Jumping around in the map editor flickers Paused to true then back to false.
+
+        // TODO: PERFORMANCE -  Idea: Read two samples of MapTime really fast back-to-back and determine paused status from 
+        //                      the difference, instead of using previous values stored in the MapTime Node (this will decrease
+        //                      the time it takes to read the paused status when the scanner interval time is set to some high value).
 
         /// <summary>
-        /// Use a timer to delay the checking of the current/previous MapTime, to fix the
-        /// "difference between 2 MapTime states" causing "flicker" between Paused=true/false.
+        /// Minimum time delay between value changing from true->false or false->true
+        /// to fix the situational "fast flicker" between Paused=true/false.
+        ///
+        /// The paused status may still update immediately (e.g. if the song was paused >= the min time, 
+        /// and then the player clicks "play", the paused status changes to false immediately).
         /// </summary>
-        private Stopwatch _timer = new Stopwatch();
+        public TimeSpan MinUpdateTime { get; set; }
+        private TimeSpan _defaultMinUpdateTime = TimeSpan.FromMilliseconds(1000);
+        private DateTime _timeOfLastValueChange = DateTime.Now;
 
-        public TimeSpan MinUpdateTime { get; set; } = StatePresenter.ScannerInterval.Add(TimeSpan.FromMilliseconds(250));
+        public PausedNode()
+        {
+            MinUpdateTime = _DeterminePausedStatusUpdateWaitTime();
+        }
 
         public override async Task<object> DetermineValueAsync()
         {
-            if (!_timer.IsRunning)
+            var currentValue = this.GetValue();
+            bool isPaused = _GetPausedStatusFromMemory();
+
+            if (currentValue is null)
             {
-                _timer.Start();
+                return await Task.FromResult(isPaused);
             }
 
-            var currentValue = this.GetValue();
+            if (this.ValueChanged())
+            {
+                _timeOfLastValueChange = DateTime.Now;
+            }
 
-            if (_timer.Elapsed.CompareTo(MinUpdateTime) < 0 && currentValue != null) // timer time less than min time
+            if (_ReadValueIsDifferentToLastSubmittedValue(currentValue, isPaused) && !_TimeSinceLastValueChangeIsGreaterThanMinUpdateTime())
             {
                 // return whatever the current value is
-                // _logger.Info($"false: {_timer.Elapsed.TotalMilliseconds}, {MinUpdateTime.TotalMilliseconds}");
+
+                //_logger.Info($"false: {TimeSinceLastValueChange().TotalMilliseconds}, {MinUpdateTime.TotalMilliseconds}");
                 return await Task.FromResult(currentValue);
             }
 
-            _timer.Restart();
+            return await Task.FromResult(isPaused);
+        }
 
+        private TimeSpan _DeterminePausedStatusUpdateWaitTime()
+        {
+            double scanTime = StatePresenter.ScannerInterval.Add(TimeSpan.FromMilliseconds(250)).TotalMilliseconds;
+            double minTime = _defaultMinUpdateTime.TotalMilliseconds;
+
+            double chosenTime = Math.Max(scanTime, minTime);
+
+            return TimeSpan.FromMilliseconds(chosenTime);
+        }
+
+        private bool _GetPausedStatusFromMemory()
+        {
             Preceders.TryGetValue(typeof(MapTimeNode), out var mapTimeNode);
             //Preceders.TryGetValue(typeof(StatusNode), out var statusNode);
 
@@ -45,16 +76,35 @@ namespace OsuStatePresenter.Nodes
             var previousMapTime = (int?)mapTimeNode?.GetPreviousValue() ?? 0;
             //var status = (string)statusNode?.GetValue();
 
-            //_logger.Info($"{previousMapTime} -> {currentMapTime}");
+            _logger.Info($"{previousMapTime} -> {currentMapTime}");
 
             //if (status.Contains("Playing") && currentMapTime.Equals(previousMapTime))
             if (currentMapTime.Equals(previousMapTime))
             {
-                return await Task.FromResult(true);
+                return true;
             }
 
-            return await Task.FromResult(false);
+            return false;
         }
 
+        private bool _ReadValueIsDifferentToLastSubmittedValue(object currentValue, bool isPausedReadFromMemory)
+        {
+            if (currentValue is null)
+            {
+                throw new Exception($"currentValue should never be null here (in {this.GetType().ToString()}).");
+            }
+
+            return !((bool)currentValue).Equals(isPausedReadFromMemory);
+        }
+
+        private bool _TimeSinceLastValueChangeIsGreaterThanMinUpdateTime()
+        {
+            return _TimeSinceLastValueChange().CompareTo(MinUpdateTime) > 0;
+        }
+
+        private TimeSpan _TimeSinceLastValueChange()
+        {
+            return DateTime.Now - _timeOfLastValueChange;
+        }
     }
 }
